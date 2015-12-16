ff519e05e205f803a23a77eb282912fb64635a86
Chi LT <lieu.thanh.chi@gmail.com>
2015-12-16 02:10:39 +0000

    20151215

---
+++ application/controllers/home/index.php
@@ -0,0 +1,15 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+class Index extends Front_Controller
+{
+	private $layout;
+	public function __construct()
+	{
+		parent::__construct();
+		$this->layout = 'layout';
+	}
+	public function index()
+	{
+		$this->viewData['_body'] = $this->load->view( $this->APP . '/index/index-body', array(), true);
+		$this->render( $this->layout );
+	}
+}
---
+++ application/views/front/layout.php
@@ -0,0 +1,2 @@
+testeinf
+<?=@$_body?>
