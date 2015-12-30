61f6171d4ccb225be74fd1387971b1a363fea869
Chi LT <lieu.thanh.chi@gmail.com>
2015-12-30 04:30:29 +0000

    ajax fixed

--- application/views/front/home/index-body.php
+++ application/views/front/home/index-body.php
@@ -49,16 +49,20 @@
 
         <li data-transition="fade" data-masterspeed="5000" data-slotamount="7" data-delay="50000" data-title="Slide 1"> <img src="<?php echo base_url($themes . 'img/bg.jpg'); ?>" data-bgposition="center center" data-bgfit="cover" data-bgrepeat="no-repeat" alt="">
           <div class="bg-overlay"></div>
+
           <div class="tp-caption sft stt" data-x="center" data-hoffset="0" data-y="center" data-voffset="-100" data-speed="600" data-start="300" data-endspeed="400" data-end="79500" data-easing="Power4.easeOut" style="z-index: 3">
             <h3 class="title-slider-small uppercased mb10">An easy way to build a Custom Web App</h3>
             <div class="br-bottom-center"></div>
           </div>
+
           <div class="tp-caption sfb stb" data-x="center" data-hoffset="0" data-y="center" data-voffset="5" data-speed="600" data-start="400" data-endspeed="400" data-end="79500" data-easing="Power4.easeOut" style="z-index: 3">
             <h2 class="title-slider-large uppercased bordered">CloudBerry Software</h2>
             <div class="title-slider-small uppercased mb10">
               <span class="typist"></span>
             </div>
           </div>
+
+
         </li>
 
       </ul>
